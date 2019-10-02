3d5ff5fce0e645643617c2f6f7160adc5d5bbdda
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-10-02 18:53:03 -0300

    Salva estado de logado ou não logado

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -3,14 +3,26 @@ import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
 import AuthLayout from '~/pages/_layouts/auth';
-// import DefaultLayout from '~/pages/_layouts/default';
+import DefaultLayout from '~/pages/_layouts/default';
+import { store } from '~/store';
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const Layout = AuthLayout;
+  const { signed } = store.getState().auth;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
   return (
     <Route
       {...rest}
